b35708ae5eef7b05238aa29690a46c76a52d4d46
CitrusFruits <duensing.andrew@gmail.com>
2015-12-08 15:32:17 -0800

    Add picklist capabilities to Progress Indicator Component

--- progressIndicator/progressIndicatorRuntime.js
+++ progressIndicator/progressIndicatorRuntime.js
@@ -2,6 +2,8 @@
 	skuid.componentType.register('progressindicator__progress_indicator', function(element, xmlDef, component){
 		var $ = skuid.jQuery;
 		var id = component.element.attr('id');
+		var friendId = xmlDef.attr('friendId');
+		var mode = xmlDef.attr('mode') || 'tabwiz';
 
 		// Our outer content div
 		var content = $('<div class="progress-indicator">');
@@ -9,20 +11,39 @@
 		var cRow = $('<tr>');
 
 		// Get all of our steps
-		var steps = xmlDef.children('steps').children();
+		var steps;
+		if(mode == 'tabwiz'){
+			steps = xmlDef.children('steps').children().map(function(index, step){
+				return {
+					dataId: step.attributes['data-id'].value,
+					icon: skuid.utils.mergeAsText('global', step.attributes.icon),
+					label: step.attributes.label.value
+				};
+			});
+		}
+		else{
+			steps = [];
+			var picklistField = skuid.$M(xmlDef.attr('model')).getField(xmlDef.attr('field'));
+
+			$.each(picklistField.picklistEntries, function(i, entry){
+				steps.push({
+					label: entry.label,
+					dataId: '' + i
+				});
+			});
+		}
 
 		// Iterate through each of our step, and create a 
 		// corresponding td element with the proper classes
 		// and content
 		for (var i = 0; i < steps.length; i++) {
-			var td = $('<td class="progress-chunk" data-id="' + steps[i].attributes['data-id'].value + '">');
+			var td = $('<td class="progress-chunk" data-id="' + steps[i].dataId + '">');
 			
 			var text = $('<div class="progress-text">');
-			var icon = steps[i].attributes.icon;
-			if(icon && icon.value){
-				text.append($('<div>').addClass('nx-step-icon ' + icon.value + ' sk-icon inline'));
+			if(steps[i].icon && steps[i].icon.value){
+				text.append($('<div>').addClass('nx-step-icon ' + steps[id].icon.value + ' sk-icon inline'));
 			}
-			text.append('<div class="text-content">' + steps[i].attributes.label.value + '</div>');
+			text.append('<div class="text-content">' + steps[i].label + '</div>');
 
 			td.append(text);
 			td.append('<div class="progress-triangle"></div>');
@@ -62,7 +83,7 @@
 
 		var updateIndex = function(index){
 			for (var i = 0; i < steps.length; i++) {
-				var td = component.element.find('[data-id="' + steps[i].attributes['data-id'].value + '"]');
+				var td = component.element.find('[data-id="' + steps[i].dataId + '"]');
 
 				// Clear out all the classes from our element
 				td.removeClass('done previous current');
@@ -92,7 +113,7 @@
 			// The friend of this components is how this
 			// component will determine when to update itself
 
-			var friendComponent = $('#' + xmlDef.attr('friendId'));
+			var friendComponent = $('#' + friendId);
 			
 			// Make sure the friend component exists
 			if(friendComponent){
@@ -106,7 +127,6 @@
 
 					// Add a listener to when the tabs are activated
 					friendComponent.on('tabsbeforeactivate', function(e, ui) {
-						console.log('ui', ui);
 						var index = ui.newTab.index();
 						updateIndex(index);
 					});
@@ -121,17 +141,43 @@
 				else{
 					// Add a listener to when the steps are progressed
 					skuid.events.subscribe('skuid.wizard.stepShow', function(data){
-						updateIndex(data.step.element.index());
+						if(data.wizard.element.attr('id') == friendId){
+							updateIndex(data.step.element.index());
+						}
 					});
 				}
 			}
+		};
+
+
+		if(mode == 'tabwiz'){
+			// This first call will call whenever the Progress Indicator is actually 
+			// rendered
+			bindFriend();
+			// This second one will be called when the page is done loading
+			$(document).one('pageload', bindFriend);
 		}
+		else{
+			if(skuid.$M(xmlDef.attr('model')).getRows()){
+				var setPicklist = function(){
+					var row = skuid.$M(xmlDef.attr('model')).getRows()[0];
+					var entries = skuid.$M(xmlDef.attr('model')).getField(xmlDef.attr('field')).picklistEntries;
+					updateIndex(entries.indexOf(row[xmlDef.attr('field')]));
+					$.each(entries, function(i, entry){
+						if(entry.label == row[xmlDef.attr('field')]){
+							updateIndex(i);
+							return false;
+						}
+					});
+				}
+				setPicklist();
+				skuid.events.subscribe('row.updated',function(updateResult){
+				 if ((updateResult.modelId === xmlDef.attr('model'))) {
+						 setPicklist();
+				 }
+			});
 
-
-		// This first call will call whenever the Progress Indicator is actually 
-		// rendered
-		bindFriend()
-		// This second one will be called when the page is done loading
-		$(document).one('pageload', bindFriend);
+			}
+		}
 	});
 })(skuid);
